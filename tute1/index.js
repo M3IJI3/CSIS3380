aa7afd962935cba176a10c488d7af5cc555266a6
Mei <meij1@student.douglascollege.ca>
2024-05-07 14:58:44 -0700

    tute1

---
+++ tute1/index.js
@@ -0,0 +1,45 @@
+// const name = 'Jesse';
+// greeting = () => alert(`Greetings! ${name}`);
+// greeting
+
+// function getArea(width, length){
+//     return width * length
+// }
+
+// getArea = (width, length) => width * length;
+
+// getArea = (width, length) => {
+//     console.log('test');
+//     return width * length;
+// };
+
+// console.log(getArea(18,20))
+
+// const nums = [11,22,33]
+// function add(first, second, third){
+//     return first + second + third
+// }
+
+// let total = add(...nums)
+// console.log(total)
+
+// const postIds = [1,2,3]
+// const newIds = [4,5,6]
+// postIds.push(...newIds)
+// console.log(...postIds)
+
+
+posts = [
+{ title: 'this no', id: 1 }, 
+{ title: 'this React', id: 2 }
+]
+
+// let newPosts = posts.map( post => render(post) )
+// function render(input){
+//     console.log(input);
+// }
+
+// let filterPosts = posts.filter( post => {
+//     return post.title.includes('ct')
+// })
+// console.log(filterPosts);

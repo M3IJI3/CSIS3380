d217dcf7f721da371473f8e291eaf86c854d9241
Jie Mei <majorangeee@gmail.com>
2024-05-07 19:30:38 -0700

    update

--- Exercise - Page Numbering - Start Files/Exercise Lab 2 - Starter/js/script.js
+++ Exercise - Page Numbering - Start Files/Exercise Lab 2 - Starter/js/script.js
@@ -21,13 +21,65 @@
 
 // showPage(contactItem, page)
 
+const perPage = 10;
+var currentPage = 1;
+
 fetch('js/users.json')
   .then(response => response.json())
   .then(data => {
-    console.log(data[0].name);
+    displayProfiles(data);
+    displayPagination(data);
+    const quantity = data.length;
+    document.getElementById('contact-amount').textContent = `Total: ${quantity}`;
   })
   .catch(error => {
     console.error('Error fetching data:', error);
   });
 
+
+function displayProfiles(data)
+{
+    const startIndex = (currentPage - 1) * perPage;
+    const endIndex = startIndex + perPage;
+    const profiles = data.slice(startIndex, endIndex);
+
+    const profileList = document.querySelector('.contact-list');
+    profileList.innerHTML = '';
+
+    profiles.forEach(profile => {
+        const profileCard = document.createElement('li');
+        profileCard.classList.add('contact-item');
+        profileCard.classList.add('cf');
+
+        profileCard.innerHTML = `
+        <div class="contact-details">
+        <img class="avatar" src="${profile.image}">
+        <h3>${profile.name}</h3>
+        </div>
+        <div class="joined-details">
+           <span class="date">Joined ${profile.joined}</span>
+        </div>`;
+
+        profileList.appendChild(profileCard);
+
+    });
+}
+
+function displayPagination(data)
+{
+    const totalPages = Math.ceil(data.length / perPage);
+    const pagination = document.querySelector('.pagination');
+    pagination.innerHTML = '';
+
+    for (let i = 1; i <= totalPages; i++) {
+        const button = document.createElement('li');
+        button.innerHTML = `<a>${i}</a>`;
+        
+        button.addEventListener('click', () => {
+            currentPage = i;
+            displayProfiles(data);
+        });
+        pagination.appendChild(button);
+    }
+}
 

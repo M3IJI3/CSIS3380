97eff9a9c0bd40d5acfb927e5e3803c4cd718eeb
Jie Mei <majorangeee@gmail.com>
2024-05-21 14:23:54 -0700

    update

--- week_3/list/app.js
+++ week_3/list/app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
 
+songs=['Sgt. Peppers Lonely Hearts Club Band',
+    'With a Little Help from My Friends',
+    'Lucy in the Sky with Diamonds',
+    'Getting Better' ,
+    'Fixing a Hole',
+    'Shes Leaving Home',
+    'Being for the Benefit of Mr. Kite!' ,
+    'Within You Without You',
+    'When Im Sixty-Four',
+    'Lovely Rita',
+    'Good Morning Good Morning',
+    'Sgt. Peppers Lonely Hearts Club Band (Reprise)',
+    'A Day in the Life'];
+
 app.use('/static', express.static('public'));
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-    res.render('list');
+    res.render('listwithloop', (songs));
 })
 
 app.listen(3000, ()=>{

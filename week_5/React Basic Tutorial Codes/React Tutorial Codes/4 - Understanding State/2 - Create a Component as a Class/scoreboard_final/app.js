7dcda15eb63f9e9620b9c362778f079aa3c5a4e3
Mei <meij1@student.douglascollege.ca>
2024-06-11 13:07:03 -0700

    week_6

--- week_5/React Basic Tutorial Codes/React Tutorial Codes/4 - Understanding State/2 - Create a Component as a Class/scoreboard_final/app.js
+++ week_5/React Basic Tutorial Codes/React Tutorial Codes/4 - Understanding State/2 - Create a Component as a Class/scoreboard_final/app.js
@@ -43,12 +43,32 @@ const Player = (props) => {
 }
 
 class Counter extends React.Component {
+  state = {
+    score: 0
+  }
+
+  incrementScore = () => {
+    this.setState({
+      score: this.state.score + 1
+    }) 
+    // console.log("Hi, this is a log form incrementScore.")
+  }
+
+  decrementScore = () => {
+    if (this.state.score > 0)
+    {
+      this.setState({
+        score: this.state.score - 1
+      })
+    }
+  }
+
   render() {
     return (
       <div className="counter">
-        <button className="counter-action decrement"> - </button>
-        <span className="counter-score">{ this.props.score }</span>
-        <button className="counter-action increment"> + </button>
+        <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
+        <span className="counter-score">{ this.state.score }</span>
+        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
       </div>
     );
   }
